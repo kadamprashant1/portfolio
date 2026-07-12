59337a9207958060200b817586287eb1500b6ac9
kadamprashant1 <97536014+kadamprashant1@users.noreply.github.com>
2026-07-12 23:54:48 +0530

    Upadate vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Root domain deployment (prashantkadam.github.io)
+  base: '/portfoil_game/', // Root domain deployment (prashantkadam.github.io)
   plugins: [
     react(),
     tailwindcss(),
